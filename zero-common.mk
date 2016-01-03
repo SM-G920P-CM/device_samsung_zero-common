670960a49f12fc5e22792b742ba0b3bb37912a86
W.D <djvoleur@gmail.com>
2016-01-02 22:12:46 -0700

    Update GPS

--- zero-common.mk
+++ zero-common.mk
@@ -98,9 +98,7 @@ PRODUCT_PACKAGES += \
     setup_fs
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps.xml:system/etc/gps.xml \
-    $(LOCAL_PATH)/configs/lhd.conf:system/etc/lhd.conf
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
@@ -157,7 +155,6 @@ PRODUCT_PACKAGES += \
     fstab.samsungexynos7420 \
     init.baseband.rc \
     init.carrier.rc \
-    init.gps.rc \
     init.samsungexynos7420.rc \
     init.samsungexynos7420.usb.rc \
     init.wifi.rc \
