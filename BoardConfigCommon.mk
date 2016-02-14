b3e62cf86eb2884affa47272f7b26cf31cf502bf
Tony Malagisi <tvm2487@gmail.com>
2016-02-13 20:19:31 -0500

    Update for the mdm9x35 modem

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,7 +100,7 @@ TARGET_SLSI_VARIANT := blobs
 
 # Radio
 BOARD_PROVIDES_LIBRIL := true
-BOARD_MODEM_TYPE := ss333
+BOARD_MODEM_TYPE := mdm9x35
 BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 
 # Recovery
